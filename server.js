3fe9d4fa9faa22dd08691ab921c2c54b7a0785a6
Dheeraj Vajjarapu <dheerajvajjarapu@Dheerajs-MacBook-Pro.local>
2021-11-09 01:16:31 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+const getColors = require('get-image-colors')
+
+
+const options = {
+    count: 100,
+
+}
+
+
+app.get('/', (req, res) => {
+
+    getColors(path.join(__dirname, 'image.jpg'),options).then(colors => {
+        res.send(colors)
+        
+    
+    })
+    
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+}) 
