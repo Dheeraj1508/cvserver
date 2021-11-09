5a7146145c19b7f87d5313d7ca1dc2cf3c6dbb2e
Dheeraj Vajjarapu <dheerajvajjarapu@Dheerajs-MacBook-Pro.local>
2021-11-09 10:37:22 +0530

    commit

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const options = {
     count: 100,
 
 }
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 
 app.post('/api', (req, res) => {
